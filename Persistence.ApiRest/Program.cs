10ef5180e12fd56541c9dd70f68f5b4b14223934
sonia <eha80877@educastur.es>
2025-05-19 21:04:35 +0200

    cambios en program y appsettings

--- Persistence.ApiRest/Program.cs
+++ Persistence.ApiRest/Program.cs
@@ -1,9 +1,26 @@
+using Persistence.ApiRest;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar la cadena de conexión
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                      ?? Environment.GetEnvironmentVariable("DATABASE_URL");
 
+// Si DATABASE_URL viene en formato Railway mysql://usuario:clave@host:puerto/db, 
+// necesitas convertirla a formato para Pomelo MySQL
+
+if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("mysql://"))
+{
+    connectionString = ConvertirDatabaseUrlToMySqlConnectionString(connectionString);
+}
+
+// Añadir DbContext con MySQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -27,3 +44,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+// Método para convertir DATABASE_URL a cadena de conexión MySQL compatible
+string ConvertirDatabaseUrlToMySqlConnectionString(string databaseUrl)
+{
+    // Ejemplo: mysql://usuario:clave@host:puerto/db
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var user = userInfo[0];
+    var password = userInfo[1];
+    var host = uri.Host;
+    var port = uri.Port;
+    var database = uri.AbsolutePath.TrimStart('/');
+
+    return $"Server={host};Port={port};Database={database};User={user};Password={password};";
+}
