b850ec73dae971f1957feb57b52a60d8ed9fd4a7
sonia <eha80877@educastur.es>
2025-06-01 22:42:16 +0200

    Nueva migracion

--- Persistence.ApiRest/Migrations/20250601203758_AddFechaRegistroToRecetas.Designer.cs
+++ Persistence.ApiRest/Migrations/20250601203758_AddFechaRegistroToRecetas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.ApiRest;
 
@@ -11,9 +12,11 @@ using Persistence.ApiRest;
 namespace Persistence.ApiRest.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601203758_AddFechaRegistroToRecetas")]
+    partial class AddFechaRegistroToRecetas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence.ApiRest/Migrations/20250601203758_AddFechaRegistroToRecetas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.ApiRest.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFechaRegistroToRecetas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "fecha_registro",
+                table: "Recetas",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fecha_registro",
+                table: "Recetas");
+        }
+    }
+}
